fc3f666ae0e69cd631d04be3c8dd401f151e1833
Alex <ivanchyshyn14@gmail.com>
2022-11-03 21:41:30 +0200

    Exercise 4: add Mobile menu modal

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import {COLORS, WEIGHTS} from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
@@ -15,23 +15,90 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay onDismiss={onDismiss}>
+      <Content>
+        <CloseButton onClick={onDismiss}>
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+          <Icon id="close" />
+        </CloseButton>
+        <Menu>
+          <MenuLink href="/sale">Sale</MenuLink>
+          <MenuLink href="/new">New&nbsp;Releases</MenuLink>
+          <MenuLink href="/men">Men</MenuLink>
+          <MenuLink href="/women">Women</MenuLink>
+          <MenuLink href="/kids">Kids</MenuLink>
+          <MenuLink href="/collections">Collections</MenuLink>
+        </Menu>
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: hsl(0deg 0% 0% / 0.5);
+`
+
+const Content = styled(DialogContent)`
+  position: relative;
+  background: white;
+  width: 266px;
+  height: 100%;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 32px;
+  padding-right: 22px;
+  overflow: auto;
+`
+
+const CloseButton = styled(UnstyledButton)`
+  margin-left: auto;
+`
+
+const Menu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  flex: 1;
+  justify-content: center;
+`
+
+const MenuLink = styled.a`
+  font-size: 1.125rem;
+  line-height: 21px;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`
+
+const FooterLink = styled.a`
+  font-size: 0.875rem;
+  text-decoration: none;
+  color: ${COLORS.gray[700]};
+  font-weight: ${WEIGHTS.normal};
+  line-height: 16px;
+`
 
 export default MobileMenu;
